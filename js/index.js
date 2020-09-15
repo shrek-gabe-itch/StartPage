0d8a9ad025dccb6a6dff9c69448e74e0b36e3645
shrek-gabe-itch <58945139+shrek-gabe-itch@users.noreply.github.com>
2020-09-16 01:21:17 +0500

    Initial commit

---
+++ js/index.js
@@ -0,0 +1,23 @@
+var grid = document.getElementById("grid");
+var sortable = new Sortable(grid, {
+	forceFallback: true,
+	sort: true,
+	animation: 250,
+	dataIdAttr: "id",
+	store: {
+		get: function (sortable) {
+  			var order = localStorage.getItem(sortable.options.group.name);
+  			return order ? order.split(",") : [];
+		},
+		set: function (sortable) {
+			var order = sortable.toArray();
+			localStorage.setItem(sortable.options.group.name, order.join(","));
+    	},
+	}
+});
+
+
+// var links = document.links;
+// for (var i = 0; i < links.length; i++) {
+//      links[i].target = "_blank";
+// }
